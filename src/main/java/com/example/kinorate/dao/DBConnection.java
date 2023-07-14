58a58a798c665995f1a52f9f1d2def942ef753ce
EDyugaeva <ed2408@yandex.ru>
2023-07-14 12:23:08 +0500

    db connectioin

---
+++ src/main/java/com/example/kinorate/dao/DBConnection.java
@@ -0,0 +1,34 @@
+package com.example.kinorate.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    public static Connection getConnectionToDataBase() {
+        Connection connection = null;
+
+        try {
+
+            //load the driver class for postgresql
+            Class.forName("org.postgresql.Driver");
+
+            connection = DriverManager.getConnection(
+                    "jdbc:postgresql://localhost:5432/FilmRating", "Ekaterina", "password");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (connection != null) {
+            System.out.println("Connection established successfully");
+        }
+        return connection;
+
+
+
+
+    }
+
+}
