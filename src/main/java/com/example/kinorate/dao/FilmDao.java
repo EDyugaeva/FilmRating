426d5e43b8b8c438194d2f504737e305003650f1
EDyugaeva <ed2408@yandex.ru>
2023-07-24 18:59:34 +0500

    film dao refactor

--- src/main/java/com/example/kinorate/dao/FilmDao.java
+++ src/main/java/com/example/kinorate/dao/FilmDao.java
@@ -11,29 +11,27 @@ import java.util.List;
 @Slf4j
 public class FilmDao {
 
+    private Connection connection = DBConnection.getConnectionToDataBase();
+
     private final FilmMapper mapper = new FilmMapper();
     private static final String FIND_BY_ID = "SELECT * FROM films WHERE id = ?";
     private static final String FIND_BY_TITLE = "SELECT * FROM films WHERE title LIKE ?";
     private static final String INSERT = "INSERT INTO films (title, description, image) VALUES (?, ?, ?)";
     private static final String FIND_TOP_5 = "SELECT * FROM films ORDER BY rate DESC LIMIT 5";
     private static final String UPDATE_RATING = "UPDATE films SET rate = ? WHERE id = ?";
-
+    private static final String DELETE_FILM_BY_ID = "DELETE from films WHERE id = ?";
 
 
     public Film findFilmById(Long filmId) {
         log.info("searching film by id {}", filmId);
         Film film = null;
-        try {
-            Connection connection = DBConnection.getConnectionToDataBase();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID)) {
 
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_ID);
             preparedStatement.setLong(1, filmId);
-
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 film = mapper.getFilm(rs);
             }
-
             return film;
 
         } catch (SQLException e) {
@@ -47,11 +45,9 @@ public class FilmDao {
         log.info("searching film by title");
         Film film = null;
         List<Film> list = new ArrayList<>();
-        try {
-            Connection connection = DBConnection.getConnectionToDataBase();
-            PreparedStatement statement = connection.prepareStatement(FIND_BY_TITLE);
-            statement.setString(1, "%" + searchString + "%");
+        try (PreparedStatement statement = connection.prepareStatement(FIND_BY_TITLE)) {
 
+            statement.setString(1, "%" + searchString + "%");
             ResultSet rs = statement.executeQuery();
 
             while (rs.next()) {
@@ -73,10 +69,8 @@ public class FilmDao {
         log.info("Creating new film");
 
         int rowsAffected = 0;
-        try {
-            Connection connection = DBConnection.getConnectionToDataBase();
+        try (PreparedStatement statement = connection.prepareStatement(INSERT)) {
 
-            PreparedStatement statement = connection.prepareStatement(INSERT);
             statement.setString(1, film.getTitle());
             statement.setString(2, film.getDescription());
             statement.setString(3, String.valueOf(film.getImage()));
@@ -94,10 +88,8 @@ public class FilmDao {
     public List<Film> findTop5Films() {
         log.info("Finding 5 top films");
         List<Film> list = new ArrayList<>();
-        try {
-            Connection connection = DBConnection.getConnectionToDataBase();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(FIND_TOP_5)) {
 
-            PreparedStatement preparedStatement = connection.prepareStatement(FIND_TOP_5);
             ResultSet rs = preparedStatement.executeQuery();
 
             while (rs.next()) {
@@ -117,10 +109,8 @@ public class FilmDao {
         log.info("Updating ratings");
         int rowsAffected = 0;
 
-        try {
-            Connection connection = DBConnection.getConnectionToDataBase();
+        try (PreparedStatement statement = connection.prepareStatement(UPDATE_RATING)) {
 
-            PreparedStatement statement = connection.prepareStatement(UPDATE_RATING);
             statement.setFloat(1, film.getRate());
             statement.setLong(2, film.getId());
 
@@ -133,6 +123,15 @@ public class FilmDao {
         return rowsAffected;
     }
 
+
+    public void deleteFilmById(Long id) {
+        try (PreparedStatement statement = connection.prepareStatement(DELETE_FILM_BY_ID)) {
+            statement.setLong(1, id);
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
 }
