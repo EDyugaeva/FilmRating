17dd7ef47ba3b684377442c2dbb9f247e3df24fd
EDyugaeva <ed2408@yandex.ru>
2023-07-24 14:40:59 +0500

    saving file changes

--- src/main/java/com/example/kinorate/utills/SavingFiles.java
+++ src/main/java/com/example/kinorate/utills/SavingFiles.java
@@ -1,25 +1,32 @@
 package com.example.kinorate.utills;
 
 import jakarta.servlet.http.Part;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
 import java.io.IOException;
 
+@Slf4j
 public class SavingFiles {
-    private static String savePath = "C:/Users/ekaterina/IdeaProjects/KinoRate/src/main/webapp/images";
+    private static final String savePath = "C:/Users/ekaterina/IdeaProjects/KinoRate/src/main/webapp/images/";
 
 
-    // Save the file to the server
+    /**
+     * Saving images to the filesystem.
+     *
+     * @param part  - data from servlet
+     * @param title - title of film/image
+     * @param type  - root where to save, there will be option to save users photo
+     * @return url to save in database
+     * @throws IOException
+     */
     public static String saveImage(Part part, String title, String type) throws IOException {
+        log.info("Saving images to folder {} ", type);
 
-        String fileName = replaceSpaces(title) + ".jpg";
-
-        System.out.println("file name" + fileName);
+        String fileName = replaceExtras(title) + ".jpg";
 
         // Specify the directory where you want to save the uploaded files
         String fullPath = savePath + type;
-
-        System.out.println("full path" + fullPath);
 
         // Create the save directory if it does not exist
         File fileSaveDir = new File(fullPath);
@@ -28,18 +35,18 @@ public class SavingFiles {
         }
         String filePath = fullPath + File.separator + fileName;
 
-        System.out.println("file Path" + filePath);
-
-        String imageUrl = type + File.separator + fileName;
+        String imageUrl = "images" + File.separator + type + File.separator + fileName;
 
         part.write(filePath);
+
+        log.info("url to save is: {} ", imageUrl);
 
         return imageUrl;
     }
 
 
-    private static String replaceSpaces(String title) {
-        return title.replaceAll("\\s+", "_");
+    private static String replaceExtras(String title) {
+        return title.replaceAll("\\s+", "_").replaceAll(":", "");
     }
 
 
