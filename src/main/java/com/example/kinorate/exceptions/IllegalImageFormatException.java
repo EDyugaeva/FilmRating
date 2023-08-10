16d9e86930552ebcc86e90e090cf3603b32378eb
EDyugaeva <ed2408@yandex.ru>
2023-08-10 16:54:01 +0500

    IllegalImageFormatException

---
+++ src/main/java/com/example/kinorate/exceptions/IllegalImageFormatException.java
@@ -0,0 +1,8 @@
+package com.example.kinorate.exceptions;
+
+public class IllegalImageFormatException extends RuntimeException {
+
+    public IllegalImageFormatException(String message) {
+        super(message);
+    }
+}
