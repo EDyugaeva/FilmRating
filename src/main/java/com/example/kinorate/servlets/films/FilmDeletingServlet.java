0e7a7707bd4204dacb4abf05b26a5e7819e3ab5e
EDyugaeva <ed2408@yandex.ru>
2023-08-10 16:54:36 +0500

    film services

--- src/main/java/com/example/kinorate/servlets/films/FilmDeletingServlet.java
+++ src/main/java/com/example/kinorate/servlets/films/FilmDeletingServlet.java
@@ -1,7 +1,8 @@
 package com.example.kinorate.servlets.films;
 
-import com.example.kinorate.dao.FilmDao;
+import com.example.kinorate.dao.impl.FilmDaoImpl;
 import com.example.kinorate.model.Film;
+import com.example.kinorate.services.FilmService;
 import com.example.kinorate.utills.FileUtills;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -11,19 +12,17 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 @WebServlet("/film-deleting")
 @Slf4j
 public class FilmDeletingServlet extends HttpServlet {
-    FilmDao dao = new FilmDao();
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         Long filmId = Long.valueOf(req.getParameter("film_id"));
         log.info("Delete film with id = {}", filmId);
-        Film film = dao.findFilmById(filmId);
-        dao.deleteFilmById(filmId);
-        FileUtills.deleteFile(film.getImage());
+        FilmService.delete(filmId);
         req.setAttribute("info", "film was deleted");
         req.getRequestDispatcher("html/OK.jsp").include(req, resp);
         req.getRequestDispatcher("/mypage").include(req, resp);
